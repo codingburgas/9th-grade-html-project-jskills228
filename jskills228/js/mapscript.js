8f64dc95b07bc39aa81043f06a6281de794c9d45
“KSGencheva23” <KSGencheva23@codingburgas.bg>
2025-06-12 11:52:55 +0300

    Make changes to the map for incidents

--- jskills228/js/mapscript.js
+++ jskills228/js/mapscript.js
@@ -11,7 +11,8 @@ const incidents = [
     type: 'Пожар',
     team: 'Екип 1',
     vehicle: 'PB9876BC',
-    time: '10.06.2025 14:32'
+    time: '10.06.2025 14:32',
+    picture: "https://static.dir.bg/uploads/images/2018/03/29/461047/orig.jpg?_=1524078351"
   },
   {
     lat: 42.1354,
@@ -19,7 +20,8 @@ const incidents = [
     type: 'Катастрофа',
     team: 'Екип 2',
     vehicle: 'CX1234AB',
-    time: '10.06.2025 15:12'
+    time: '10.06.2025 15:12',
+    picture: "https://i.id24.bg/i/2110236.jpg"
   },
   {
     lat: 43.2141,
@@ -27,7 +29,8 @@ const incidents = [
     type: 'Наводнение',
     team: 'Екип 3',
     vehicle: 'VN5678CD',
-    time: '09.06.2025 09:00'
+    time: '09.06.2025 09:00',
+    picture: "https://static.meteobalkans.com/meteo/images/2024/09/11/5785/1280X720-459093982_8711863642180374_1242772351322039528_n.jpg"
   }
 ];
 
@@ -37,6 +40,7 @@ incidents.forEach(incident => {
     <strong>Тип:</strong> ${incident.type}<br>
     <strong>Екип:</strong> ${incident.team}<br>
     <strong>Автомобил:</strong> ${incident.vehicle}<br>
-    <strong>Време:</strong> ${incident.time}
+    <strong>Време:</strong> ${incident.time}<br>
+    <img src="${incident.picture}" alt="${incident.type}" style="width: 100% ; max-width: 400px; border-radius: 10px; margin-top:20px;">
   `);
 });
